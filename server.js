146bd77b65c6d989035f14d9732da96a816559b9
Andrew Garcia <andrew@drwgrc.dev>
2022-03-10 14:13:57 -0500

    cluster performance and zero downtime restart

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    // event loop is blocked...
+  }
+}
+
+app.get('/', (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.stringify("{}") => {}
+  // [5,1,2,3,4].sort()
+  res.send(`Performance example: ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+  delay(9000);
+  res.send(`Beep beep beep! ${process.pid}`);
+});
+
+console.log('Running server.js ...');
+console.log('Worker process started.');
+app.listen(3000);
